254494c9f5231e9ef0e75de48b0f850035be83b7
Ben Mills <fuzzyllama@gmail.com>
2022-09-21 11:31:17 -0700

    Changes default battle to include random weapons

--- app/src/main/java/minibattle/MiniBattle.java
+++ app/src/main/java/minibattle/MiniBattle.java
@@ -2,13 +2,16 @@ package minibattle;
 
 import minibattle.battle.Battle;
 import minibattle.creature.Creature;
+import minibattle.weapon.Weapon;
 
 public class MiniBattle {
 
     public static void main(String[] args) {
-        Creature a = new Creature("Potato Knight");
-        Creature b = new Creature("Onion Knight");
-        Battle battle = new Battle(a, b);
+        Weapon weaponA = new Weapon(); // Random weapon
+        Weapon weaponB = new Weapon(); // Random weapon
+        Creature creatureA = new Creature("Potato Knight", weaponA);
+        Creature creatureB = new Creature("Onion Knight", weaponB);
+        Battle battle = new Battle(creatureA, creatureB);
         battle.duel();
     }
 }
