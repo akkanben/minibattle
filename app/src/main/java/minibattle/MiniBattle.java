7584208e6bb4b04eb40271896b676dabff320d83
Ben Mills <fuzzyllama@gmail.com>
2022-10-01 11:17:25 -0700

    Refactors minibattle to launch a single elemination game -- also adds static scanner, random and name getter instances

--- app/src/main/java/minibattle/MiniBattle.java
+++ app/src/main/java/minibattle/MiniBattle.java
@@ -1,54 +1,32 @@
 package minibattle;
 
-import minibattle.battle.Battle;
 import minibattle.creature.Creature;
-import minibattle.weapon.Weapon;
+import minibattle.creature.name.NameGetter;
+import minibattle.game.SingleElimination;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class MiniBattle {
 
+    private static final Random random = new Random();
+    private static final NameGetter nameGetter = new NameGetter();
+    private static final Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        Creature creatureA = new Creature("Potato Knight", new Weapon());
-        Creature creatureB = new Creature("Onion Knight", new Weapon());
-        Battle battle;
-        String[] mobs = {"MobA", "MobB", "MobC"};
-        Creature selectedCreature;
-        int roundCount = 3;
-        for(int i = 0 ; i < roundCount; i++) {
-            battle = new Battle(creatureA, creatureB);
-            System.out.println(battle.printStatus());
-            int selection;
-            do {
-                System.out.println("Please select your creature.");
-                System.out.print("Enter 1 for " + creatureA.getName() + " or 2 for " + creatureB.getName() + ":");
-                selection = scanner.nextInt();
-                scanner.nextLine(); // to consume \n
-            } while (selection != 1 && selection != 2);
-            if (selection == 1) {
-                System.out.println("\nYou selected " + creatureA.getName());
-                selectedCreature = creatureA;
-                creatureB = new Creature(mobs[i]);
-            } else {
-                System.out.println("\nYou selected " + creatureB.getName());
-                selectedCreature = creatureB;
-                creatureA = new Creature(mobs[i]);
-            }
-            System.out.println("\nPress Enter to Continue");
-            scanner.nextLine();
-            Creature result = battle.duel(scanner);
-            if (result.equals(selectedCreature)) {
-                System.out.println("You selected the winner.");
-                System.out.println("Moving on to the next round");
-            } else {
-                System.out.println("You failed to select the winner. Game Over.");
-                System.exit(0);
-            }
-            System.out.println("\nPress Enter to Continue");
-            scanner.nextLine();
-        }
-        System.out.println("\nCongratulations!!! You made it to the end.");
-        scanner.close();
+        SingleElimination game = new SingleElimination(3);
+        game.start();
+    }
+
+    public static Random random() {
+        return random;
+    }
+
+    public static String getCreatureName(Creature.Stat affinity) {
+       return nameGetter.getName(affinity);
+    }
+
+    public static Scanner inputScanner() {
+        return scanner;
     }
 }
